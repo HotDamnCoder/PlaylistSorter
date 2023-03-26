25d0de314b2698fc7322edd260a555823c15e269
Marcus Bindevald <mabind@ttu.ee>
2023-03-26 16:53:47 +0300

    Removed secrets

--- src/assets/TS/credentials.ts
+++ src/assets/TS/credentials.ts
@@ -1,12 +1,12 @@
 const YOUTUBE_CLIENT_ID =
-  "1044735142013-k3bmr7kq5qn4ijdbs6jj211ipj5p2lbv.apps.googleusercontent.com";
+  "***REMOVED***";
 const YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube";
 const YOUTUBE_DISCOVERY_DOCS = [
   "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest",
 ];
 
-const SPOTIFY_CLIENT_SECRET = "ca3a458dd41f4412bd76ff862edb2b8f";
-const SPOTIFY_CLIENT_ID = "a264ff02b1f24d179ffd96680d556f56";
+const SPOTIFY_CLIENT_SECRET = "***REMOVED***";
+const SPOTIFY_CLIENT_ID = "***REMOVED***";
 const SPOTIFY_REDIRECT_URI = "http://localhost:8080/callback";
 const SPOTIFY_SCOPE =
   "user-read-private playlist-modify-private playlist-read-private";
